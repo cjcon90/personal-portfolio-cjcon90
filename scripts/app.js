c79b538ce5087d3b7306e5dbb498ab32c22a3fbe
Ciaran Concannon <cjcon90@protonmail.com>
2020-09-12 23:54:56 +0100

    added project cards & social links

--- scripts/app.js
+++ scripts/app.js
@@ -6,3 +6,9 @@ document.querySelectorAll('a[href^="#"]').forEach((elem) => {
       });
    });
 });
+
+ScrollReveal().reveal(".skills__heading", { delay: 300, duration: 1000 });
+ScrollReveal().reveal(".profile", { duration: 1000 });
+ScrollReveal().reveal(".skills__block", { delay: 800, interval: 200 });
+ScrollReveal().reveal(".projects__heading", { duration: 1000 });
+ScrollReveal().reveal(".projects__card", { delay: 200, duration: 1000 });
