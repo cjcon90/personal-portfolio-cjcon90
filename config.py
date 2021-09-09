5d3a8286c8a5f927b5065b61a2c9c20505e6ecd4
Ciaran Concannon <cjcon90@pm.me>
2021-09-09 01:21:11 +0100

    fix error in SMTP settings

--- config.py
+++ config.py
@@ -8,11 +8,10 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
     # Email configuration
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    MAIL_DEFAULT_SENDER = ('Ciaran Concannon', 'cjcon90@gmail.com')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['cjcon90@gmail.com', 'cjcon90@pm.me']
