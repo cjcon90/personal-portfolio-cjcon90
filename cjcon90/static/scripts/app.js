d1d28421dfe5e6b67f0fb88986e358854e166130
Ciaran Concannon <cjcon90@pm.me>
2021-09-10 14:52:58 +0100

    add loading spinner for email

--- cjcon90/static/scripts/app.js
+++ cjcon90/static/scripts/app.js
@@ -18,29 +18,39 @@ ScrollReveal().reveal(".projects__card", { delay: 200, duration: 1000 });
 
 //CONTACT FORM FUNCTIONALITY
 
-const name = document.getElementById("name");
+const nameField = document.getElementById("name");
 const nameError = document.getElementById("name-error");
 const email = document.getElementById("email");
 const emailError = document.getElementById("email-error");
+const mailReg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const message = document.getElementById("message");
 const messageError = document.getElementById("message-error");
 const submit = document.getElementById("submit");
-const mailReg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const loadingOverlay = document.querySelector(".loading-overlay");
+let nameCheck, emailCheck, messageCheck;
 
 submit.addEventListener("click", (e) => {
-   name.value.trim().length > 0 ? (nameError.style.opacity = "0") : ((nameError.style.opacity = "1"), e.preventDefault());
+   nameField.value.trim().length > 0 ? (nameError.style.opacity = "0", nameCheck = 1) : ((nameError.style.opacity = "1"), e.preventDefault(), nameCheck = 0);
 
    if (email.value.trim() === "") {
       emailError.textContent = "What's your email?";
       emailError.style.opacity = "1";
       e.preventDefault();
+      emailCheck = 0;
    } else if (!mailReg.test(email.value)) {
       emailError.textContent = "Hmm... this doesn't look right.";
       emailError.style.opacity = "1";
       e.preventDefault();
+      emailCheck = 0;
    } else {
       emailError.style.opacity = "0";
+      emailCheck = 1;
    }
 
-   message.value.trim().length > 0 ? (messageError.style.opacity = "0") : ((messageError.style.opacity = "1"), e.preventDefault());
+   message.value.trim().length > 0 ? (messageError.style.opacity = "0", messageCheck = 1) : ((messageError.style.opacity = "1"), e.preventDefault(), messageCheck = 0);
+
+   if (nameCheck && emailCheck && messageCheck) {
+      loadingOverlay.classList.remove("no-display");
+      loadingOverlay.classList.add("grid-center", "loading-overlay-appear");
+   }
 });
